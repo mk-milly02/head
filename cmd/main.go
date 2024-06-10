8d1df0a0fa9057d2f57a3f14ed62ef5d2bc1c782
Jonas Ababio <jonasagesmilly@outlook.com>
2024-06-10 19:39:55 +0000

    Add a function that reads input from a file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
-	
+
+}
+
+func read_from_file(filename string) []byte {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("%s: no such file or directory", filename)
+	}
+	defer file.Close()
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return content
 }
